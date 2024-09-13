af8c4e25de2c21255ec022d4c843daa36e0ef73e
alucardrueda2021 <alucardrueda2021@gmail.com>
2024-09-13 16:33:47 +0000

    Update repo

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,8 +9,10 @@
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
+    pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
+    pkgs.gitflow-cli
+    pkgs.sudo
   ];
 
   # Sets environment variables in the workspace
